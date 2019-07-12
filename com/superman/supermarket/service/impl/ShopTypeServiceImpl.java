a1be941cc29ec10487f4db05b163508acad66905
GXH-gxh <2378545787@qq.com>
2019-07-12 17:53:21 +0800

    和第二次的架构响应

---
+++ com/superman/supermarket/service/impl/ShopTypeServiceImpl.java
@@ -0,0 +1,20 @@
+package com.superman.supermarket.service.impl;
+
+import com.superman.supermarket.entity.ShopType;
+import com.superman.supermarket.dao.ShopTypeMapper;
+import com.superman.supermarket.service.ShopTypeService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author superman
+ * @since 2019-07-12
+ */
+@Service
+public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements ShopTypeService {
+
+}
